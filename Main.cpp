361dd9efc8969d38bd9d59624e38cfbba764920b
U-BSD\299747 <299747@SHS-10DD3C429.beaverton.k12.or.us>
2017-02-23 14:29:08 -0800

    hi

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,27 @@
 // Main.cpp
+
+#include <iostream>
+#include "Node.h"
+
+int main(){
+  String infix;
+  cout<<"Enter your expression"<<endl;
+  cin.getline(infix);
+  cin.ignore();
+  String postfix = convert(infix);
+  cout<<postfix<<endl;
+}
+
+String convert(string infix){
+  String postfix = "";
+  for(int i=0; i<infix.length(); i++){
+
+  }
+  return postfix;
+}
+
+bool isOperator (char character){
+  if(C=='+'||C=='-'||C=='*'||C=='/'||C=='^'||C=='('||C==')'){
+    return true;
+  }
+}
